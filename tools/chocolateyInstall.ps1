434cd262f3e40c4e00e38d14becf18ccfa46f7ed
Lukasz Rajchel <lukasz@rajchel.pl>
2016-08-12 09:24:40 +0200

    [skip ci] URL updated

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'Line6Monkey'
 $installerType = 'EXE'
-$url = 'http://fr.line6.com/getrelease?rid=6460'
+$url = 'http://fr.line6.com/getrelease?rid=6996'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url
